3243eb530cb04d19c8b47579289e780433d70274
Wesley Moskal-Fitzpatrick <github@fitzmoskal.me>
2025-07-08 11:25:16 +0100

    Add git requirement check

--- dependency-hell.sh
+++ dependency-hell.sh
@@ -7,6 +7,12 @@ then
     exit
 fi
 
+# Ensure git is installed
+if ! command -v git >/dev/null; then
+    echo "git is required" >&2
+    exit 1
+fi
+
 # Initialize optional parameters
 DESIRED_K8S_VERSION=""
 DESIRED_PYTHON_VERSION=""
